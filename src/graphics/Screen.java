f4291ba682e90ca82fa9ebc7e20cf9f09ac90254
Carlin Au <carlinau@gmail.com>
2016-12-24 07:44:07 -0500

    Out of Bounds Ep11

--- src/graphics/Screen.java
+++ src/graphics/Screen.java
@@ -6,7 +6,7 @@ public class Screen
 	private int width;
 	private int height;
 	public int[] pixels;
-	public int time= 0, counter=0;
+	public int xtime=0, ytime=50, counter=0;
 	
 	// Screen Constructor
 	public Screen(int width, int height)
@@ -20,26 +20,29 @@ public class Screen
 	// We need something to flush the buffer so that it can update whats on the screen
 	public void clear() {
 		for (int i = 0; i < pixels.length; i++) {
-			pixels[i-1] = 0;
+			pixels[i] = 0;
 		}
 	}
 	
 	public void render() {
 		counter++;
-		if(counter % 100 == 0) {
-			time++;
-		}
+		if(counter % 10 == 0) xtime++;
+		if(counter % 80 == 0) ytime++;
 		// Nested for loops to iterate through all the pixels in the Screen
 		// Renders Vertically (Top Down)
 		for(int y = 0; y<height; y++)
 		{
+			// handle ArrayOutOfBounds Exception
+			if (ytime >= height) break;
 			for(int x = 0; x<width; x++)
 			{
+				// handle ArrayOutOfBounds Exception
+				if (xtime >= width) break;
 				// Changes pixels to be magenta?
 				// x + y * width is a way to access the pixels from a single dimension array
 				// width accounts for the width of the pixels in a screen; y accounts for the row.
 				// width acts as a displacement for the rows of pixels in the one dimensional array.
-				pixels[time+time*width] = 0xff00ff;
+				pixels[xtime+ytime*width] = 0xff00ff;
 			}
 		}
 	}
